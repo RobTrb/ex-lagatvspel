82ead50f839763c009473ca9e3f21355304a0058
RobTrb <Robert.trb@gmail.com>
2026-01-15 22:01:38 +0100

    added more content on homepage and made design adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,54 @@
 import styles from "../styles/home.module.css";
-import Image from "next/image";
+import InfoBoxTopHome from "@/components/InfoBoxTopHome";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <img src="/img/Banner-large.webp" alt="LagaTVSpel banner" className={styles.banner} />      
-      <main className={styles.main}>        
-        <h1>Welcome to LagaTVSpel</h1>
-        <p>Gaming console and controller repair services</p>
+      <div className={styles.bannerContainer}>
+        <img
+          src="/img/Banner-large.webp"
+          alt="LagaTVSpel banner futuristic consoles"
+          className={styles.banner}
+        />
+        <div className={styles.infoBoxOverlay}>
+          <InfoBoxTopHome />
+        </div>
+      </div>
+      <main className={styles.main}>
+        <div className={styles.servicesContainer}>
+          <div className={styles.servicesContent}>
+            <h1>Våra tjänster</h1>
+            <p>Upptäck vårt utbud av tjänster</p>
+            <h2>Konsolreparation</h2>
+            <p>
+              Snabb och pålitlig reparation anpassad efter dina behov. Vi
+              reparerar och ersätter trasiga kompenenter på{" "}
+              <strong>Nintendo Switch, Playstation och XBOX</strong>. Vi
+              åtgärdar problem som exempelvis överhettning och trasiga{" "}
+              <strong>HDMI-portar</strong>.
+            </p>
+            <h2>Moddning och chippning</h2>
+            <p>
+              Modifierar och installerar{" "}
+              <strong>chip för anpassad och förbättrad spelupplevelse</strong>.
+            </p>
+          </div>
+          <div className={styles.servicesImgContainer}>
+            <img
+              src="/img/longpsctrl.jpg"
+              alt="Image of disassembled playstation controller"
+            ></img>
+          </div>
+        </div>
       </main>
+      <div className={styles.bannerContainer}>
+        <img
+          src="/img/Banner-large-broken.webp"
+          alt="LagaTVSpel banner broken consoles"
+          className={styles.banner}
+        />
+      </div>
+      <main className={styles.main}></main>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,21 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "@/styles/headerFooter.module.css";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
   return (
     <header className={styles.headerContainer}>
       <div className={styles.header}>
-        <img src="/img/LOGO-150x150.webp" alt="LagaTVSpel logo" className={styles.logo} />
+        <img
+          src="/img/LOGO-150x150.webp"
+          alt="LagaTVSpel logo"
+          className={styles.logo}
+        />
         <h1 className={styles.headerTitle}>LAGATVSPEL</h1>
         <button
           className={styles.hamburger}
@@ -24,19 +30,52 @@ export default function Header() {
         <nav className={`${styles.nav} ${isOpen ? styles.navOpen : ""}`}>
           <ul className={styles.navList}>
             <li>
-              <Link href="/">Hem</Link>
+              <Link
+                href="/"
+                className={pathname === "/" ? styles.active : styles.inactive}
+              >
+                Hem
+              </Link>
             </li>
             <li>
-              <Link href="/inspiration">Inspiration</Link>
+              <Link
+                href="/inspiration"
+                className={
+                  pathname === "/inspiration" ? styles.active : styles.inactive
+                }
+              >
+                Inspiration
+              </Link>
             </li>
             <li>
-              <Link href="/priser">Priser</Link>
+              <Link
+                href="/priser"
+                className={
+                  pathname === "/priser" ? styles.active : styles.inactive
+                }
+              >
+                Priser
+              </Link>
             </li>
             <li>
-              <Link href="/kontakt">Kontakt</Link>
+              <Link
+                href="/kontakt"
+                className={
+                  pathname === "/kontakt" ? styles.active : styles.inactive
+                }
+              >
+                Kontakt
+              </Link>
             </li>
             <li>
-              <Link href="/tidsbokning">Boka tid</Link>
+              <Link
+                href="/tidsbokning"
+                className={
+                  pathname === "/tidsbokning" ? styles.active : styles.inactive
+                }
+              >
+                Boka tid
+              </Link>
             </li>
           </ul>
         </nav>
